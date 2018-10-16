9bbe59e09e54435a99ceb6b44dfbc1287c1c079e
Brian Carroll <brian.carroll.ireland@gmail.com>
2018-10-16 23:21:21 +0100

    Code comments

--- src/kernel/types.c
+++ src/kernel/types.c
@@ -3,13 +3,13 @@
 #include <string.h>
 #include "types.h"
 
-
+// Global constants that don't need headers
 u8 False = 0;
 u8 True = 1;
 u8 Unit = 0;
 
 struct nil Nil = {
-    .header = HEADER_NIL  // not on the dynamic heap, but has a header anyway
+    .header = HEADER_NIL
 };
 
 Cons* newCons(void* head, void* tail) {
@@ -59,32 +59,21 @@ ElmChar* newElmChar(u32 value) {
     return p;
 };
 
+// Strings are padded to the next 32-bit boundary.
+// Last byte of padding is a number 1-4 telling the size of the padding itself,
+// so that we can find the exact byte length of the String.
+// Roughly like OCaml, https://v1.realworldocaml.org/v1/en/html/memory-representation-of-values.html#string-values
 ElmString* newElmString(size_t n, char *str) {
-    // Pad to next 32-bit boundary
     size_t n_ints = n/4 + 1;
     size_t n_bytes_padded = n_ints * 4;
 
     ElmString *p = malloc(sizeof(ElmString) + n_bytes_padded);
     p->header = HEADER_STRING(n_ints);
 
-    // Last byte of the padding contains its size in bytes
     u32* ints = (u32*)p->bytes;
-    u32 padding = (n_bytes_padded - n) << 24; // padding size, with 3 bytes of leading zeros (little-endian)
+    u32 padding = (n_bytes_padded - n) << 24; // little-endian => high byte goes last
     ints[n_ints-1] = padding;
 
-    // Copy string content (overwrites some of the padding and that's OK)
     memcpy(p->bytes, str, n);
     return p;
 }
-
-// // Sanity check
-// void main(void) {
-//     newCons(&Unit, &Nil);
-//     newTuple2(&Unit, &Unit);
-//     newTuple3(&Unit, &Unit, &Unit);
-//     newElmInt(123);
-//     newElmFloat(123.456);
-//     newElmChar('A');
-//     newElmString(11, "hello world");
-//     printf("OK\n");
-// }
