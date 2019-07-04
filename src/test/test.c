70bf4d3d1e1cc77974c9c74e398f4d1a93dea9fa
Brian Carroll <brian.carroll.ireland@gmail.com>
2019-07-04 06:50:53 +0100

    Make verbose tests more verbose

--- src/test/test.c
+++ src/test/test.c
@@ -19,36 +19,37 @@
 #include "../kernel/gc.h"
 
 #ifdef __EMSCRIPTEN__
-    int verbose = true;
+int verbose = true;
 #else
-    int verbose = false;
+int verbose = false;
 #endif
 
 int tests_run = 0;
-
+int assertions_made = 0;
 
 // Debug function, with pre-allocated memory for strings
 // Avoiding use of malloc in test code in case it screws up GC
 // A single printf may require many separate hex strings
 #define TEST_MAX_HEXES_PER_PRINTF 16
-char hex_strings[TEST_MAX_HEXES_PER_PRINTF][9*1024/4];
-char* hex(void* addr, int size) {
+char hex_strings[TEST_MAX_HEXES_PER_PRINTF][9 * 1024 / 4];
+char *hex(void *addr, int size)
+{
     static u32 rotate = 0;
-    rotate = (rotate+1) % TEST_MAX_HEXES_PER_PRINTF;
+    rotate = (rotate + 1) % TEST_MAX_HEXES_PER_PRINTF;
 
     size = (size < 1024) ? size : 1024;
-    u32 i, c=0;
-    for (i=0; i < size; c += 9, i += 4) {
+    u32 i, c = 0;
+    for (i = 0; i < size; c += 9, i += 4)
+    {
         // Print in actual byte order (little endian)
         sprintf(hex_strings[rotate] + c,
-            "%02x%02x%02x%02x|",
-            *(u8*)(addr + i),
-            *(u8*)(addr + i + 1),
-            *(u8*)(addr + i + 2),
-            *(u8*)(addr + i + 3)
-        );
+                "%02x%02x%02x%02x|",
+                *(u8 *)(addr + i),
+                *(u8 *)(addr + i + 1),
+                *(u8 *)(addr + i + 2),
+                *(u8 *)(addr + i + 3));
     }
-    hex_strings[rotate][c-1] = 0; // erase last "|" and terminate the string
+    hex_strings[rotate][c - 1] = 0; // erase last "|" and terminate the string
     return hex_strings[rotate];
 }
 
@@ -56,31 +57,34 @@ char* hex(void* addr, int size) {
 //   Addresses are printed in little-endian format
 //   => Easier to see what pointers are pointing to it
 //   => Harder to subtract in your head if you're interested in sizes & memory layout
-char* hex_ptr(void* ptr) {
-    return hex(&ptr, sizeof(void*));
+char *hex_ptr(void *ptr)
+{
+    return hex(&ptr, sizeof(void *));
 }
 
-
-
-char* test_all(
+char *test_all(
     bool types,
     bool utils,
     bool basics,
     bool string,
-    bool gc
-) {
-    if (types)  mu_run_test(types_test);
-    if (utils)  mu_run_test(utils_test);
-    if (basics) mu_run_test(basics_test);
-    if (string) mu_run_test(string_test);
-    if (gc)     mu_run_test(gc_test);
+    bool gc)
+{
+    if (types)
+        mu_run_test(types_test);
+    if (utils)
+        mu_run_test(utils_test);
+    if (basics)
+        mu_run_test(basics_test);
+    if (string)
+        mu_run_test(string_test);
+    if (gc)
+        mu_run_test(gc_test);
 
     return NULL;
 }
 
-
-
-int main(int argc, char ** argv) {
+int main(int argc, char **argv)
+{
 
     GC_init();
     Types_init();
@@ -89,16 +93,15 @@ int main(int argc, char ** argv) {
     String_init();
 
     static struct option long_options[] =
-    {
-        {"verbose", no_argument, NULL, 'v'},
-        {"all", no_argument, NULL, 'a'},
-        {"types", no_argument, NULL, 't'},
-        {"utils", no_argument, NULL, 'u'},
-        {"basics", no_argument, NULL, 'b'},
-        {"string", no_argument, NULL, 's'},
-        {"gc", no_argument, NULL, 'g'},
-        {NULL, 0, NULL, 0}
-    };
+        {
+            {"verbose", no_argument, NULL, 'v'},
+            {"all", no_argument, NULL, 'a'},
+            {"types", no_argument, NULL, 't'},
+            {"utils", no_argument, NULL, 'u'},
+            {"basics", no_argument, NULL, 'b'},
+            {"string", no_argument, NULL, 's'},
+            {"gc", no_argument, NULL, 'g'},
+            {NULL, 0, NULL, 0}};
 
     // By default in Bash shell, just do what's specified
     bool types = false;
@@ -107,72 +110,77 @@ int main(int argc, char ** argv) {
     bool utils = false;
     bool gc = false;
 
-    // Running in a Windows CMD shell
-    // Prob via Codelite IDE. Dunno how to set up CLI args there yet!
-    // Set this up for whatever I'm working on
-    #if defined(_WIN32)
+// Running in a Windows CMD shell
+// Prob via Codelite IDE. Dunno how to set up CLI args there yet!
+// Set this up for whatever I'm working on
+#if defined(_WIN32)
     gc = true;
     verbose = true;
-    #endif
+#endif
 
-    // In the browser. No CLI. Set reasonable defaults.
-    #if defined(__EMSCRIPTEN__)
+// In the browser. No CLI. Set reasonable defaults.
+#if defined(__EMSCRIPTEN__)
     types = true;
     utils = true;
     basics = true;
     string = true;
     verbose = true;
-    #endif
+#endif
 
     int opt;
-    while ((opt = getopt_long(argc, argv, "vatubsg", long_options, NULL)) != -1) {
-        switch (opt) {
-            case 'v':
-                verbose = true;
-                break;
-            case 'a':
-                types = true;
-                utils = true;
-                basics = true;
-                string = true;
-                gc = true;
-                break;
-            case 't':
-                types = true;
-                break;
-            case 'u':
-                utils = true;
-                break;
-            case 'b':
-                basics = true;
-                break;
-            case 's':
-                string = true;
-                break;
-            case 'g':
-                gc = true;
-                break;
-            default:
-                fprintf(stderr, "Usage: %s [-vatubsg]\n", argv[0]);
-                exit(EXIT_FAILURE);
+    while ((opt = getopt_long(argc, argv, "vatubsg", long_options, NULL)) != -1)
+    {
+        switch (opt)
+        {
+        case 'v':
+            verbose = true;
+            break;
+        case 'a':
+            types = true;
+            utils = true;
+            basics = true;
+            string = true;
+            gc = true;
+            break;
+        case 't':
+            types = true;
+            break;
+        case 'u':
+            utils = true;
+            break;
+        case 'b':
+            basics = true;
+            break;
+        case 's':
+            string = true;
+            break;
+        case 'g':
+            gc = true;
+            break;
+        default:
+            fprintf(stderr, "Usage: %s [-vatubsg]\n", argv[0]);
+            exit(EXIT_FAILURE);
         }
     }
 
-    char* result = test_all(
+    char *result = test_all(
         types,
         utils,
         basics,
         string,
-        gc
-    );
+        gc);
     bool passed = (result == NULL);
 
-    if (!passed) {
+    if (!passed)
+    {
         printf("Failed: %s\n", result);
-    } else {
+    }
+    else
+    {
         printf("\nALL TESTS PASSED\n");
     }
     printf("Tests run: %d\n", tests_run);
+    printf("Assertions made: %d\n", assertions_made);
 
     int exit_code = passed ? EXIT_SUCCESS : EXIT_FAILURE;
     exit(exit_code);
--- src/test/test.h
+++ src/test/test.h
@@ -1,10 +1,30 @@
 #define __USE_MINGW_ANSI_STDIO 1
 #include <stdio.h>
 
-#define mu_assert(message, test) do { if (!(test)) return message; } while (0)
-#define mu_run_test(test) do { char *message = test(); tests_run++; \
-                                if (message != NULL) return message; } while (0)
-extern int tests_run, verbose;
+#define mu_assert(message, test)     \
+  do                                 \
+  {                                  \
+    assertions_made++;               \
+    if (!(test))                     \
+    {                                \
+      return message;                \
+    }                                \
+    else if (verbose)                \
+    {                                \
+      printf("PASS: %s\n", message); \
+    }                                \
+  } while (0)
 
-char* hex(void* addr, int size);
-char* hex_ptr(void* ptr);
+#define mu_run_test(test)   \
+  do                        \
+  {                         \
+    char *message = test(); \
+    tests_run++;            \
+    if (message != NULL)    \
+      return message;       \
+  } while (0)
+
+extern int tests_run, assertions_made, verbose;
+
+char *hex(void *addr, int size);
+char *hex_ptr(void *ptr);
