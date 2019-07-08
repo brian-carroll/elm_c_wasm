19f67f1e3c89b4a6c55e5687d286be8c485739f9
Brian Carroll <brian.carroll.ireland@gmail.com>
2019-07-08 21:36:33 +0100

    Restore code deleted developing stackmap test

--- src/kernel/utils.c
+++ src/kernel/utils.c
@@ -131,12 +131,7 @@ void *Utils_apply(Closure *c_old, u8 n_applied, void *applied[])
 
     void *push = CAN_THROW(GC_stack_push());
 #ifdef DEBUG
-    // gc_debug_stack_trace(push, c);
-    // printf("Utils_apply evaluating:\n");
-    // printf("    closure = %p\n", c);
-    // for (size_t a=0; a<c->max_values; a++) {
-    //     printf("    arg[%zd] = %p (%x)\n", a, args[a], ((Header*)(args[a]))->tag);
-    // }
+    gc_debug_stack_trace(push, c);
 #endif
 
     ElmValue *result = (*c->evaluator)(args);
@@ -147,7 +142,7 @@ void *Utils_apply(Closure *c_old, u8 n_applied, void *applied[])
 
     void *pop = CAN_THROW(GC_stack_pop(result, push));
 #ifdef DEBUG
-    // gc_debug_stack_trace(pop, c);
+    gc_debug_stack_trace(pop, c);
 #else
     (void)pop; // suppress "unused variable" warning
 #endif
