192f9d00a05863581e39baf4e9e81d84ca6c586b
Brian Carroll <brian.carroll.ireland@gmail.com>
2019-08-06 11:36:19 +0300

    Benchmark boxed vs unboxed counter

---
+++ demos/benchmark/src/Main.elm
@@ -0,0 +1,45 @@
+module Main exposing (main)
+
+import Benchmark exposing (..)
+import Benchmark.Runner exposing (BenchmarkProgram, program)
+
+
+type Box a
+    = Box a
+
+
+main : BenchmarkProgram
+main =
+    program suite
+
+
+suite : Benchmark
+suite =
+    let
+        count =
+            100 * 1000
+    in
+    describe "Count"
+        [ benchmark "unboxed" <|
+            \_ -> countUnboxed count
+        , benchmark "boxed" <|
+            \_ -> countBoxed (Box count)
+        ]
+
+
+countUnboxed : Int -> ()
+countUnboxed remaining =
+    if remaining == 0 then
+        ()
+
+    else
+        countUnboxed (remaining - 1)
+
+
+countBoxed : Box Int -> ()
+countBoxed (Box remaining) =
+    if remaining == 0 then
+        ()
+
+    else
+        countBoxed (Box (remaining - 1))
