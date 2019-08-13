83133fc0b050eb00c87e78fd02e998a7efaf1e66
Brian Carroll <brian.carroll.ireland@gmail.com>
2019-08-13 23:56:49 +0300

    Remove Emscripten wrapping
    2x speedup on unboxed addition

--- demos/benchmark/src/patch.js
+++ demos/benchmark/src/patch.js
@@ -1,17 +1,5 @@
 //============ start patch.js ===================
-var author$project$Main$wasmCount = function wasmCount(remaining) {
-  return Module.ccall('export_count', 'number', ['number'], [remaining]);
-};
-var author$project$Main$wasmAdd = F2(function wasmAdd(a, b) {
-  return Module.ccall('export_add', 'number', ['number', 'number'], [a, b]);
-});
-var author$project$Main$wasmAddUnboxed = F2(function wasmAddUnboxed(a, b) {
-  return Module.ccall(
-    'export_add_unboxed',
-    'number',
-    ['number', 'number'],
-    [a, b]
-  );
-});
-
+var author$project$Main$wasmCount = Module.asm._export_count;
+var author$project$Main$wasmAdd = F2(Module.asm._export_add);
+var author$project$Main$wasmAddUnboxed = F2(Module.asm._export_add_unboxed);
 //============ end patch.js ===================
