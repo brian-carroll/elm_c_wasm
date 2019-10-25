96a17feff6c1610921f15f74889e299c72c4f7d2
Brian Carroll <brian.carroll.ireland@gmail.com>
2019-10-25 17:31:38 +0100

    Reorganise elm-without-kernel

--- demos/wrapper/list-kernel-fns.sh
+++ demos/wrapper/list-kernel-fns.sh
@@ -1,1 +1,2 @@
-egrep -o '_[A-Z][a-zA-Z_]+_[a-zA-Z_]+' build/elm.js | sort | uniq | grep -v UNUSED
+filepath=$1
+egrep -o '_[A-Z][a-zA-Z_]+_[a-zA-Z_]+' $filepath | sort | uniq | grep -v UNUSED
--- demos/wrapper/src/elm-without-kernel.js
+++ demos/wrapper/src/elm-without-kernel.js
@@ -92,9 +92,10 @@ var _VirtualDom_on = F2(function(key, handler) {
 });
 
 /***************************************
-    WASM KERNEL
+    ELM LIBRARIES
 ****************************************/
 
+// Basic types
 var _List_Nil = { $: '[]' };
 var _Utils_Tuple0 = { $: '#0' };
 var _String_fromNumber = function(x) {
@@ -107,16 +108,22 @@ var _List_fromArray = function() {
   return { $: '::', arguments: Array.prototype.slice.call(arguments) };
 };
 
-/***************************************
-    INIT
-****************************************/
+// String
+var elm$core$String$fromInt = _String_fromNumber;
 
+// Platform
+var elm$core$Platform$Cmd$batch = _Platform_batch;
+var elm$core$Platform$Cmd$none = elm$core$Platform$Cmd$batch(_List_Nil);
+var elm$core$Platform$Sub$batch = _Platform_batch;
+var elm$core$Platform$Sub$none = elm$core$Platform$Sub$batch(_List_Nil);
+
+// Task
 var elm$core$Task$succeed = _Scheduler_succeed;
 var elm$core$Task$command = _Platform_leaf('Task');
+var elm$core$Task$andThen = _Scheduler_andThen;
 var elm$core$Task$Perform = function(a) {
   return { $: 'Perform', a: a };
 };
-var elm$core$Task$andThen = _Scheduler_andThen;
 var elm$core$Task$map = F2(function(func, taskA) {
   return A2(
     elm$core$Task$andThen,
@@ -126,12 +133,15 @@ var elm$core$Task$map = F2(function(func, taskA) {
     taskA
   );
 });
+
+// Time
 var elm$time$Time$Posix = function(a) {
   return { $: 'Posix', a: a };
 };
 var elm$time$Time$millisToPosix = elm$time$Time$Posix;
-var author$project$Main$GotTime = function(a) {
-  return { $: 'GotTime', a: a };
+var elm$time$Time$posixToMillis = function(_n0) {
+  var millis = _n0.a;
+  return millis;
 };
 var elm$time$Time$now = _Time_now(elm$time$Time$millisToPosix);
 var elm$core$Task$perform = F2(function(toMessage, task) {
@@ -139,56 +149,22 @@ var elm$core$Task$perform = F2(function(toMessage, task) {
     elm$core$Task$Perform(A2(elm$core$Task$map, toMessage, task))
   );
 });
-var author$project$Main$cmdTime = A2(
-  elm$core$Task$perform,
-  author$project$Main$GotTime,
-  elm$time$Time$now
-);
 
-var author$project$Main$init = function(_n0) {
-  return _Utils_Tuple2(0, author$project$Main$cmdTime);
-};
-
-/***************************************
-    UPDATE
-****************************************/
-
-var elm$core$Platform$Cmd$batch = _Platform_batch;
-var elm$core$Platform$Cmd$none = elm$core$Platform$Cmd$batch(_List_Nil);
-
-var elm$time$Time$posixToMillis = function(_n0) {
-  var millis = _n0.a;
-  return millis;
-};
-
-var author$project$Main$update = F2(function(msg, model) {
-  if (msg.$ === 'GetTime') {
-    return _Utils_Tuple2(model, author$project$Main$cmdTime);
-  } else {
-    var posix = msg.a;
-    return _Utils_Tuple2(
-      elm$time$Time$posixToMillis(posix),
-      elm$core$Platform$Cmd$none
-    );
-  }
-});
-
-/***************************************
-    VIEW
-****************************************/
-
+// Json
 var elm$json$Json$Decode$succeed = _Json_succeed;
-var author$project$Main$GetTime = { $: 'GetTime' };
-var elm$html$Html$div = _VirtualDom_node('div');
-var elm$html$Html$br = _VirtualDom_node('br');
-var elm$html$Html$button = _VirtualDom_node('button');
+
+// VirtualDom
 var elm$virtual_dom$VirtualDom$text = _VirtualDom_text;
-var elm$html$Html$text = elm$virtual_dom$VirtualDom$text;
+var elm$virtual_dom$VirtualDom$on = _VirtualDom_on;
 var elm$virtual_dom$VirtualDom$Normal = function(a) {
   return { $: 'Normal', a: a };
 };
-var elm$virtual_dom$VirtualDom$on = _VirtualDom_on;
-var elm$core$String$fromInt = _String_fromNumber;
+
+// Html
+var elm$html$Html$br = _VirtualDom_node('br');
+var elm$html$Html$button = _VirtualDom_node('button');
+var elm$html$Html$div = _VirtualDom_node('div');
+var elm$html$Html$text = elm$virtual_dom$VirtualDom$text;
 var elm$html$Html$Events$on = F2(function(event, decoder) {
   return A2(
     elm$virtual_dom$VirtualDom$on,
@@ -204,6 +180,36 @@ var elm$html$Html$Events$onClick = function(msg) {
   );
 };
 
+/***************************************
+    PROGRAM
+****************************************/
+
+var author$project$Main$GotTime = function(a) {
+  return { $: 'GotTime', a: a };
+};
+var author$project$Main$cmdTime = A2(
+  elm$core$Task$perform,
+  author$project$Main$GotTime,
+  elm$time$Time$now
+);
+
+var author$project$Main$init = function(_n0) {
+  return _Utils_Tuple2(0, author$project$Main$cmdTime);
+};
+
+var author$project$Main$update = F2(function(msg, model) {
+  if (msg.$ === 'GetTime') {
+    return _Utils_Tuple2(model, author$project$Main$cmdTime);
+  } else {
+    var posix = msg.a;
+    return _Utils_Tuple2(
+      elm$time$Time$posixToMillis(posix),
+      elm$core$Platform$Cmd$none
+    );
+  }
+});
+
+var author$project$Main$GetTime = { $: 'GetTime' };
 var author$project$Main$view = function(model) {
   return A2(
     elm$html$Html$div,
@@ -223,15 +229,24 @@ var author$project$Main$view = function(model) {
 };
 
 /***************************************
-    SUBSCRIPTIONS
+    WRAPPER
 ****************************************/
 
-var elm$core$Platform$Sub$batch = _Platform_batch;
-var elm$core$Platform$Sub$none = elm$core$Platform$Sub$batch(_List_Nil);
+const jsKernelFunctions = [
+  _Json_succeed,
+  _Platform_batch,
+  _Platform_leaf,
+  _Scheduler_andThen,
+  _Scheduler_succeed,
+  _Time_now,
+  _VirtualDom_node,
+  _VirtualDom_on,
+  _VirtualDom_text
+];
 
-/***************************************
-    MAIN
-****************************************/
+const ctors = ['GetTime', 'GotTime', 'Normal', 'Perform', 'Posix'];
+const fields = ['init', 'subscriptions', 'update', 'view'];
+const fieldGroupNames = ['init$subscriptions$update$view'];
 
 var author$project$Main$main = author$project$WasmWrapper$element({
   init: author$project$Main$init,
