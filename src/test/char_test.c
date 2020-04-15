317587e4ef88c114495b8d0f4c19bc79f0d982c8
Brian Carroll <brian.carroll.ireland@gmail.com>
2020-04-15 18:45:56 +0100

    Char.toCode test

--- src/test/char_test.c
+++ src/test/char_test.c
@@ -12,33 +12,36 @@ han     𤭢  0x24B62     0xD852  0xDF62
 */
 
 char* test_Char_toCode() {
-  // ElmChar dollar = (ElmChar){
-  //     .header = HEADER_CHAR,
-  //     .value = 0x0024,  // $
-  // };
-  // ElmInt* dollar_code = A1(&Char_toCode, &dollar);
-  // mu_expect_equal("toCode \"$\" == 24", dollar_code->value, 0x0024);
+  ElmChar dollar = (ElmChar){
+      .words16 = {0x0000, 0x0024},  // $
+      .header = HEADER_CHAR,
+  };
+  ElmInt* dollar_code = A1(&Char_toCode, &dollar);
+  mu_expect_equal("toCode '$' == 0x24", dollar_code->value, 0x0024);
 
-  // ElmChar euro = (ElmChar){
-  //     .header = HEADER_CHAR,
-  //     .value = 0x20AC,  // €
-  // };
-  // ElmInt* euro_code = A1(&Char_toCode, &euro);
-  // mu_expect_equal("toCode euro == 0x20AC", euro_code->value, 0x20AC);
 
-  // ElmChar deseret = (ElmChar){
-  //     .header = HEADER_CHAR,
-  //     .value = 0x10437,  // 𐐷
-  // };
-  // ElmInt* deseret_code = A1(&Char_toCode, &deseret);
-  // mu_expect_equal("toCode deseret == 0xDC37D801", deseret_code->value, 0xDC37D801);
+  ElmChar euro = (ElmChar){
+      .words16 = {0x0000, 0x20AC},  // €
+      .header = HEADER_CHAR,
+  };
+  ElmInt* euro_code = A1(&Char_toCode, &euro);
+  mu_expect_equal("toCode euro == 0x20AC", euro_code->value, 0x20AC);
 
-  // ElmChar han = (ElmChar){
-  //     .header = HEADER_CHAR,
-  //     .value = 0x24B62,  // 𤭢
-  // };
-  // ElmInt* han_code = A1(&Char_toCode, &han);
-  // mu_expect_equal("toCode han == 0xDF62D852", han_code->value, 0xDF62D852);
+
+  ElmChar deseret = (ElmChar){
+      .words16 = {0xD801, 0xDC37},  // 𐐷
+      .header = HEADER_CHAR,
+  };
+  ElmInt* deseret_code = A1(&Char_toCode, &deseret);
+  mu_expect_equal("toCode deseret == 0x10437", deseret_code->value, 0x10437);
+
+
+  ElmChar han = (ElmChar){
+      .words16 = {0xD852, 0xDF62},  // 𤭢
+      .header = HEADER_CHAR,
+  };
+  ElmInt* han_code = A1(&Char_toCode, &han);
+  mu_expect_equal("toCode han == 0x24B62", han_code->value, 0x24B62);
 
   return NULL;
 }
