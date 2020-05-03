70a4557bfb1041b41c03a7fa2b3c7e501cd6352e
Brian Carroll <brian.carroll.ireland@gmail.com>
2020-05-03 10:36:28 +0100

    Create demo directory for todo-mvc

---
+++ demos/todo-mvc/build-wasm.sh
@@ -0,0 +1,30 @@
+FILENAME='elm'
+KERNELS=$(find ../../src/kernel -name '*.c')
+
+extra_opt=""
+# extra_opt="$extra_opt -DDEBUG"
+# extra_opt="$extra_opt -DDEBUG_LOG"
+extra_opt="$extra_opt -O1"
+
+extra_opt="$extra_opt -g4"
+extra_opt="$extra_opt --source-map-base http://localhost:8080/demos/2020-04-elm-spa-example/"
+
+mkdir -p build/emscripten
+
+set -x
+
+emcc build/emscripten/$FILENAME.c \
+  $KERNELS \
+  --js-library ../../src/kernel/wrapper/imports.js \
+  -o build/emscripten/$FILENAME.js \
+  -Wno-incompatible-pointer-types \
+  -s MODULARIZE_INSTANCE=1 \
+  -s ASSERTIONS=1 \
+  -s EXPORT_NAME='EmscriptenModule' \
+  -s EXPORTED_FUNCTIONS="$(cat ./exported-functions.json)" \
+  -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' \
+  -ferror-limit=10000 \
+  $extra_opt \
+  &> emcc.log
+
+set +x
---
+++ demos/todo-mvc/readme.md
@@ -0,0 +1,5 @@
+## Evan Czaplicki's TODO MVC example, compiled to WebAssembly
+
+Serves as a vehicle to develop and debug the code generator, and motivation to expand the set of C kernel functions.
+
+Evan's repo is a Git submodule of this one, so I can use the original source code. I just change the build system and the index.html file.
