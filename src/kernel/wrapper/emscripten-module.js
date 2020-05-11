c12ad7d371f9f3ca593084b5658a5beea3784899
Brian Carroll <brian.carroll.ireland@gmail.com>
2020-05-11 20:27:29 +0100

    tweak emscripten print

--- src/kernel/wrapper/emscripten-module.js
+++ src/kernel/wrapper/emscripten-module.js
@@ -1,15 +1,19 @@
+// Customise the Emscripten implementation of C printf
+// For a rapid burst of printf calls, buffer them and call console.log just once at the end.
+// Otherwise it's impractically slow to print the debug representation of the Wasm heap.
+// The debug string is about 20-25x the Wasm heap size. Typically a few MB.
 Module.print = (function () {
-  var printBuffer = '';
-  var printTimeoutId;
+  let printBuffer = '';
+  let printTimeoutId;
   return function (text) {
-    if (arguments.length > 1) {
-      text = Array.prototype.slice.call(arguments).join(' ');
+    for (let i = 1; i < arguments.length; i++) {
+      text += ' ' + arguments[i];
     }
     printBuffer += text + '\n';
     clearTimeout(printTimeoutId);
     printTimeoutId = setTimeout(() => {
       console.log(printBuffer);
       printBuffer = '';
-    }, 300);
+    }, 100);
   };
 })();
