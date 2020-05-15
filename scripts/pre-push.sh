b7800079e9214c9cc553b4c7126efa959004ac8f
Brian Carroll <brian.carroll.ireland@gmail.com>
2020-05-15 22:48:11 +0100

    Better console output in pre-push script

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,11 +1,12 @@
+#!/bin/bash
 set -e
 
 echo "Build & test binary and Wasm in parallel"
 
-(make dist/bin/test ; ./dist/bin/test --all) &
+(make dist/bin/test ; echo -e "\nBinary tests" ; ./dist/bin/test --all) &
 job_bin=$!
 
-(make www ; node ./src/test/test-runner.js --all) &
+(make www ; echo -e "\nWasm tests" ; node ./src/test/test-runner.js --all) &
 job_www=$!
 
 wait $job_bin
