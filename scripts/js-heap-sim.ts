98dcca47142c009943e2dc8e052c687464317b9b
Brian Carroll <brian.carroll.ireland@gmail.com>
2020-05-26 23:36:49 +0100

    complete simulation of JS/Wasm GC

--- scripts/js-heap-sim.ts
+++ scripts/js-heap-sim.ts
@@ -1,83 +1,111 @@
 /**
  * Simulation of JS and Wasm heaps
- *
- * Wasm program whirrs along, filling its heap
- * On every timestep it may
- *   - conjure up a JS value
- *   - copy an existing JS Ref
- *   - generate some other Wasm value
- *
- * When the Wasm heap is full
- * Mark a random selection of Wasm values
- * If it's a JS ref then tell JS it's alive
+ * Maintain a JS store of values that Wasm has references to
+ * Garbage-collect it along with Wasm
  */
+
+// --------------------------------------------------
+// Simulated Wasm module
+// --------------------------------------------------
 
 interface WasmJsRef {
   tag: 'JsRef';
   id: number;
-  marked?: boolean;
+  mark: boolean;
 }
 interface WasmValue {
   tag: 'WasmValue';
-  marked?: boolean;
+  mark: boolean;
 }
 type WasmHeapValue = WasmJsRef | WasmValue;
 
-interface JsHeapItem {
-  value: number | null;
-  marked: boolean;
-}
+const WASM_HEAP_SIZE = 100;
+let wasmHeap: WasmHeapValue[] = [];
 
-const WASM_HEAP_VALUES = 100;
-const wasmHeap: WasmHeapValue[] = [];
-const jsHeap: JsHeapItem[] = [];
-
-for (let addr = 0; addr < WASM_HEAP_VALUES; addr++) {
-  const rand = Math.random();
-  if (rand < 0.1) {
-    // create a JS Ref
-    const id = getValueFromJs();
-    wasmHeap.push({ tag: 'JsRef', id });
-  } else if (rand < 0.3) {
-    // copy a JS Ref if any exist, otherwise create one
-    const jsRefs = wasmHeap.filter(val => val.tag === 'JsRef');
-    const picked = jsRefs[
-      Math.floor(jsRefs.length * Math.random())
-    ] as WasmJsRef;
-    const id = picked ? picked.id : getValueFromJs();
-    wasmHeap.push({ tag: 'JsRef', id });
-  } else {
-    wasmHeap.push({ tag: 'WasmValue' });
+function runWasmApp() {
+  for (let addr = wasmHeap.length; addr < WASM_HEAP_SIZE; addr++) {
+    const rand = Math.random();
+    if (rand < 0.1) {
+      // create a JS Ref
+      const id = getValueFromJs();
+      wasmHeap.push({ mark: false, tag: 'JsRef', id });
+    } else if (rand < 0.3) {
+      // copy a JS Ref if any exist, otherwise create one
+      const jsRefs = wasmHeap.filter(val => val.tag === 'JsRef');
+      const picked = jsRefs[
+        Math.floor(jsRefs.length * Math.random())
+      ] as WasmJsRef;
+      const id = picked ? picked.id : getValueFromJs();
+      wasmHeap.push({ mark: false, tag: 'JsRef', id });
+    } else {
+      wasmHeap.push({ mark: false, tag: 'WasmValue' });
+    }
   }
 }
-
 function markWasm() {
   wasmHeap.forEach(val => {
     if (Math.random() < 0.4) {
-      val.marked = true;
+      val.mark = true;
       if (val.tag === 'JsRef') {
         markJs(val.id);
       }
+    } else {
+      val.mark = false;
     }
   });
 }
+function compactWasm() {
+  wasmHeap = wasmHeap.filter(val => val.mark);
+}
+
+// --------------------------------------------------
+// Actual JS heap implementation
+// --------------------------------------------------
+
+interface JsHeapEntry {
+  id: number;
+  value: number | {};
+  mark: boolean;
+}
+const unusedJsHeapSlot = {};
+const jsHeap: JsHeapEntry[] = [];
 
 function getValueFromJs(): number {
   const value = Math.random();
-  return jsHeap.push({ marked: false, value }) - 1;
+  let id = jsHeap.findIndex(entry => entry.value === unusedJsHeapSlot);
+  if (id === -1) {
+    id = jsHeap.length;
+    jsHeap.push({ id, mark: false, value });
+  } else {
+    jsHeap[id].value = value;
+  }
+  return id;
 }
 function markJs(id: number) {
-  jsHeap[id].marked = true;
+  jsHeap[id].mark = true;
 }
 function sweepJs() {
+  let lastMarked = 0;
   jsHeap.forEach((val, index) => {
-    if (!val.marked) {
-      jsHeap[index].value = null;
+    if (!val.mark) {
+      jsHeap[index].value = unusedJsHeapSlot;
+    } else {
+      lastMarked = index;
     }
+    val.mark = false;
   });
+  jsHeap.splice(lastMarked + 1, jsHeap.length);
 }
 
-markWasm();
-sweepJs();
+// --------------------------------------------------
+// Run the simulation
+// --------------------------------------------------
 
-console.log({ wasmHeap, jsHeap });
+for (let cycle = 0; cycle < 10; cycle++) {
+  runWasmApp();
+  console.log('run', { /*wasmHeap,*/ jsHeap });
+  markWasm();
+  sweepJs();
+  compactWasm();
+  console.log('GC', { /*wasmHeap,*/ jsHeap });
+}
