b33a01a20b509ccf6d3a0ecb3635fdbf9f5171c7
Brian Carroll <brian.carroll.ireland@gmail.com>
2020-06-28 18:48:20 +0100

    Stash uncommitted changes before pre-push tests and restore after

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -e
 
+# Stash changes before running tests & unstash on exit
+
+stash_stdout=$(git stash --include-untracked)
+function cleanup() {
+  if [ "$stash_stdout" != "No local changes to save" ]
+  then
+    git stash pop
+  fi
+}
+trap cleanup EXIT
+
+
+# Run tests for native and wasm concurrently
+
 (make dist/bin/test ; ./dist/bin/test --all) &
 job_bin=$!
 
