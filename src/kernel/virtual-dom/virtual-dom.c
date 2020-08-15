8bba57415656b621388fea80ae447a33ae27ed1e
Brian Carroll <brian.carroll.ireland@gmail.com>
2020-08-15 17:52:39 +0100

    Start work on virtual-dom

---
+++ src/kernel/virtual-dom/virtual-dom.c
@@ -0,0 +1,287 @@
+#include "../core/core.h"
+
+enum {
+  VDOM_TEXT,
+  VDOM_NODE,
+  VDOM_KEYED_NODE,
+  VDOM_CUSTOM,
+  VDOM_TAGGER,
+  VDOM_THUNK,
+};
+
+enum {
+  FACT_EVENT,
+  FACT_STYLE,
+  FACT_PROP,
+  FACT_ATTR,
+  FACT_ATTR_NS,
+};
+
+enum {
+  PATCH_REDRAW,
+  PATCH_FACTS,
+  PATCH_TEXT,
+  PATCH_THUNK,
+  PATCH_TAGGER,
+  PATCH_REMOVE_LAST,
+  PATCH_APPEND,
+  PATCH_REMOVE,
+  PATCH_REORDER,
+  PATCH_CUSTOM,
+};
+
+
+// Make all VirtualDom structs look like Elm Custom type to the GC
+// We don't need all 32 bits for ctor, and we have an integer field,
+// so steal some ctor bits and unbox the integer!
+// Keeping things small fits more nodes in cache, and we want to go fast.
+#define VDOM_BASE_FIELDS \
+  Header header; \
+  u8 ctor : 3; \
+  u32 descendantsCount : 29;   // < 536,870,912 descendants
+
+
+
+// TEXT
+
+
+typedef struct {
+  VDOM_BASE_FIELDS
+  ElmString16* text;
+} VdomText;
+
+static void* eval_VirtualDom_text(void* args[]) {
+  ElmString16* text = args[0];
+  VdomText* p = CAN_THROW(GC_malloc(sizeof(VdomText)));
+  *p = (VdomText){
+    .header = HEADER_CUSTOM(1),
+    .ctor = VDOM_TEXT,
+    .descendantsCount = 0,
+    .text = text,
+  };
+  return p;
+}
+
+Closure VirtualDom_text = {
+    .header = HEADER_CLOSURE(0),
+    .max_values = 1,
+    .evaluator = &eval_VirtualDom_text,
+};
+
+
+// NODE
+
+typedef struct {
+  VDOM_BASE_FIELDS
+  ElmString16* tag;
+  Custom* facts; // object
+  Custom* kids;  // array
+  ElmString16* namespace;
+} VdomNode;
+
+typedef VdomNode VdomKeyedNode;
+
+static Custom* VirtualDom_organizeFacts(Cons* factList) {
+  return NULL;
+}
+
+static void* eval_VirtualDom_nodeNS(void* args[]) {
+  ElmString16* namespace = args[0];
+  ElmString16* tag = args[1];
+  Cons* factList = args[2];
+  Cons* kidList = args[3];
+
+  u32 descendantsCount = 0;
+  Custom* kidArray = CAN_THROW(GC_malloc(sizeof(Custom)));
+  for (; kidList != &Nil; kidList = kidList->tail) {
+    CAN_THROW(GC_malloc(sizeof(void*)));
+    kidArray->header.size++;
+    VdomNode* kid = kidList->head;
+    kidArray->values[descendantsCount] = kid;
+    descendantsCount += kid->descendantsCount + 1;
+  }
+
+  VdomNode* p = CAN_THROW(GC_malloc(sizeof(VdomNode)));
+  *p = (VdomNode){
+    .header = HEADER_CUSTOM(4),
+    .ctor = VDOM_NODE,
+    .descendantsCount = descendantsCount,
+    .namespace = namespace,
+    .tag = tag,
+    .facts = VirtualDom_organizeFacts(factList),
+    .kids = kidArray,
+  };
+  return p;
+}
+
+Closure VirtualDom_nodeNS = {
+    .header = HEADER_CLOSURE(4),
+    .max_values = 4,
+    .evaluator = &eval_VirtualDom_nodeNS,
+};
+
+Closure VirtualDom_node = {
+    .header = HEADER_CLOSURE(4),
+    .n_values = 1,
+    .max_values = 3,
+    .evaluator = &eval_VirtualDom_nodeNS,
+    .values = {NULL},
+};
+
+
+// KEYED NODE
+
+static void* eval_VirtualDom_keyedNodeNS(void* args[]) {
+  ElmString16* namespace = args[0];
+  ElmString16* tag = args[1];
+  Cons* factList = args[2];
+  Cons* kidList = args[3];
+
+  u32 descendantsCount = 0;
+  Custom* kidArray = CAN_THROW(GC_malloc(sizeof(Custom)));
+  for (; kidList != &Nil; kidList = kidList->tail) {
+    CAN_THROW(GC_malloc(sizeof(void*)));
+    kidArray->header.size++;
+    VdomNode* kid = kidList->head;
+    kidArray->values[descendantsCount] = kid;
+    descendantsCount += kid->descendantsCount + 1;
+  }
+
+  VdomNode* p = CAN_THROW(GC_malloc(sizeof(VdomNode)));
+  *p = (VdomNode){
+    .header = HEADER_CUSTOM(4),
+    .ctor = VDOM_KEYED_NODE,
+    .descendantsCount = descendantsCount,
+    .namespace = namespace,
+    .tag = tag,
+    .facts = VirtualDom_organizeFacts(factList),
+    .kids = kidArray,
+  };
+  return p;
+}
+
+Closure VirtualDom_keyedNodeNS = {
+    .header = HEADER_CLOSURE(4),
+    .max_values = 4,
+    .evaluator = &eval_VirtualDom_keyedNodeNS,
+};
+
+Closure VirtualDom_keyedNode = {
+    .header = HEADER_CLOSURE(4),
+    .n_values = 1,
+    .max_values = 3,
+    .evaluator = &eval_VirtualDom_keyedNodeNS,
+    .values = {NULL},
+};
+
+// CUSTOM
+// doesn't seem to actually be used anywhere in elm org packages
+
+typedef struct {
+  VDOM_BASE_FIELDS
+  Cons* facts;
+  void* model;
+  void* render;
+  void* diff;
+} VdomCustom;
+
+
+
+// MAP
+
+typedef struct {
+  VDOM_BASE_FIELDS
+  Closure* tagger;
+  VdomNode* node;
+} VdomTagger;
+
+static void* eval_VirtualDom_map(void* args[]) {
+  Closure* tagger = args[0];
+  VdomNode* node = args[1];
+  VdomTagger* p = CAN_THROW(GC_malloc(sizeof(VdomTagger)));
+  *p = (VdomTagger){
+    .header = HEADER_CUSTOM(2),
+    .ctor = VDOM_TAGGER,
+    .descendantsCount = 1 + node->descendantsCount,
+    .tagger = tagger,
+    .node = node,
+  };
+  return p;
+}
+
+Closure VirtualDom_map = {
+    .header = HEADER_CLOSURE(0),
+    .max_values = 2,
+    .evaluator = &eval_VirtualDom_map,
+};
+
+
+// LAZY
+
+typedef struct {
+  VDOM_BASE_FIELDS
+  VdomNode* node;
+  void* refs[];
+} VdomThunk;
+
+static void* eval_VirtualDom_thunk(void* args[]) {
+  Closure* func = args[0];
+  size_t n_refs = 1 + func->max_values - func->n_values;
+
+  VdomThunk* p = CAN_THROW(GC_malloc(sizeof(VdomThunk) + n_refs * sizeof(void*)));
+  p->header = HEADER_CUSTOM(1 + n_refs);
+  p->ctor = VDOM_THUNK;
+  p->node = NULL;
+  for (size_t i = 0; i < n_refs; ++i) {
+    p->refs[i] = args[i];
+  }
+  return p;
+}
+
+Closure VirtualDom_lazy = {
+    .evaluator = &eval_VirtualDom_thunk,
+    .header = HEADER_CLOSURE(0),
+    .max_values = 2,
+};
+
+Closure VirtualDom_lazy2 = {
+    .evaluator = &eval_VirtualDom_thunk,
+    .header = HEADER_CLOSURE(0),
+    .max_values = 3,
+};
+
+Closure VirtualDom_lazy3 = {
+    .evaluator = &eval_VirtualDom_thunk,
+    .header = HEADER_CLOSURE(0),
+    .max_values = 4,
+};
+
+Closure VirtualDom_lazy4 = {
+    .evaluator = &eval_VirtualDom_thunk,
+    .header = HEADER_CLOSURE(0),
+    .max_values = 5,
+};
+
+Closure VirtualDom_lazy5 = {
+    .evaluator = &eval_VirtualDom_thunk,
+    .header = HEADER_CLOSURE(0),
+    .max_values = 6,
+};
+
+Closure VirtualDom_lazy6 = {
+    .evaluator = &eval_VirtualDom_thunk,
+    .header = HEADER_CLOSURE(0),
+    .max_values = 7,
+};
+
+Closure VirtualDom_lazy7 = {
+    .evaluator = &eval_VirtualDom_thunk,
+    .header = HEADER_CLOSURE(0),
+    .max_values = 8,
+};
+
+Closure VirtualDom_lazy8 = {
+    .evaluator = &eval_VirtualDom_thunk,
+    .header = HEADER_CLOSURE(0),
+    .max_values = 9,
+};
