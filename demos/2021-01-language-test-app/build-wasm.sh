72c4ea6eead3c0eedbf0f234cfdf9041842dacd1
Brian Carroll <brian.carroll.ireland@gmail.com>
2021-01-04 22:32:31 +0000

    new demo app for language tests

---
+++ demos/2021-01-language-test-app/build-wasm.sh
@@ -0,0 +1,31 @@
+FILENAME='elm'
+KERNELS=$(find ../../src/kernel -name '*.c')
+
+extra_opt=""
+extra_opt="$extra_opt -DDEBUG"
+# extra_opt="$extra_opt -DDEBUG_LOG"
+extra_opt="$extra_opt -O1"
+
+extra_opt="$extra_opt -g4"
+extra_opt="$extra_opt --source-map-base http://localhost:8080/demos/$(basename $(pwd))/"
+
+mkdir -p build/emscripten
+
+set -x
+
+emcc build/emscripten/$FILENAME.c \
+  $KERNELS \
+  --pre-js ../../src/kernel/wrapper/emscripten-module.js \
+  --js-library ../../src/kernel/wrapper/imports.js \
+  -o build/emscripten/$FILENAME.js \
+  -Wno-incompatible-pointer-types \
+  -s MODULARIZE_INSTANCE=1 \
+  -s ASSERTIONS=1 \
+  -s EXPORT_NAME='EmscriptenModule' \
+  -s EXPORTED_FUNCTIONS="$(cat ./exported-functions.json)" \
+  -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' \
+  -ferror-limit=10000 \
+  $extra_opt \
+  &> emcc.log
+
+set +x
