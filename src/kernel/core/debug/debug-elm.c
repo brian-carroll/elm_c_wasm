465c6436783f18ab46cfa4013dd08410ac0e06a4
Brian Carroll <brian.carroll.ireland@gmail.com>
2021-05-05 19:25:49 +0100

    Use new string builder in Debug module

--- src/kernel/core/debug/debug-elm.c
+++ src/kernel/core/debug/debug-elm.c
@@ -6,77 +6,31 @@
 
 #include <stdio.h> // putchar
 
-// helpers for string building & memory management
 
-
-size_t toString_alloc_chunk_words;
-
-struct string_builder {
-  ElmString16* s;
-  u16* cursor;
-  u16* end;
-};
-typedef struct string_builder StringBuilder;
-
-// assumes nothing else gets allocated while stringifying
-static void grow(StringBuilder* sb) {
-  GC_allocate(false, toString_alloc_chunk_words);
-  sb->end = GC_allocate(false, 0);
-  sb->s->header.size += toString_alloc_chunk_words;
-  if (toString_alloc_chunk_words < 256) toString_alloc_chunk_words *= 2;
-  return;
-}
-
-
-static void ensure_space(size_t need, StringBuilder* sb) {
-  while (sb->end - sb->cursor < need) {
-    grow(sb);
-  }
-}
-
-
-static void copy_ascii(char* src, StringBuilder* sb) {
-  char* from = src;
-  u16* to = sb->cursor;
-
-  for (; *from; to++, from++) {
-    if (to >= sb->end) {
-      grow(sb);
-    }
-    *to = *from;
-  }
-
-  sb->cursor = to;
-}
-
-
-// ========================================================
-
-
-void Debug_toStringHelp(int depth, void* p, StringBuilder* sb) {
+void Debug_toStringHelp(int depth, void* p, StrBuilder* sb) {
   ElmValue* v = p;
   char ascii_buf[25];
 
   if (!depth) {
-    copy_ascii("...", sb);
+    StrBuilder_copyAscii(sb, "...");
     return;
   }
 
   switch (v->header.tag) {
     case Tag_Int: {
       stbsp_snprintf(ascii_buf, sizeof(ascii_buf), "%d", v->elm_int.value);
-      copy_ascii(ascii_buf, sb);
+      StrBuilder_copyAscii(sb, ascii_buf);
       return;
     }
     case Tag_Float: {
       stbsp_snprintf(ascii_buf, sizeof(ascii_buf), "%.16g", v->elm_float.value);
-      copy_ascii(ascii_buf, sb);
+      StrBuilder_copyAscii(sb, ascii_buf);
       return;
     }
     case Tag_Char: {
       u16 lower = v->elm_char.words16[0];
       u16 upper = v->elm_char.words16[1];
-      ensure_space(upper ? 4 : 3, sb);
+      StrBuilder_ensureSpace(sb, upper ? 4 : 3);
       u16* write = sb->cursor;
       *write++ = '\'';
       *write++ = lower;
@@ -87,7 +41,7 @@ void Debug_toStringHelp(int depth, void* p, StringBuilder* sb) {
     }
     case Tag_String: {
       size_t len = code_units(&v->elm_string16);
-      ensure_space(len + 2, sb);
+      StrBuilder_ensureSpace(sb, len + 2);
       u16* write = sb->cursor;
       *write++ = '"';
       for (size_t i = 0; i < len; ++i) {
@@ -98,44 +52,44 @@ void Debug_toStringHelp(int depth, void* p, StringBuilder* sb) {
       return;
     }
     case Tag_List: {
-      copy_ascii("[", sb);
+      StrBuilder_copyAscii(sb, "[");
       for (Cons* list = &v->cons; list != pNil; list = list->tail) {
         Debug_toStringHelp(depth - 1, list->head, sb);
-        if (list->tail != pNil) copy_ascii(", ", sb);
+        if (list->tail != pNil) StrBuilder_copyAscii(sb, ", ");
       }
-      copy_ascii("]", sb);
+      StrBuilder_copyAscii(sb, "]");
       return;
     }
     case Tag_Tuple2: {
       Tuple2* t = &v->tuple2;
-      copy_ascii("(", sb);
+      StrBuilder_copyAscii(sb, "(");
       Debug_toStringHelp(depth - 1, t->a, sb);
-      copy_ascii(", ", sb);
+      StrBuilder_copyAscii(sb, ", ");
       Debug_toStringHelp(depth - 1, t->b, sb);
-      copy_ascii(")", sb);
+      StrBuilder_copyAscii(sb, ")");
       return;
     }
     case Tag_Tuple3: {
       Tuple3* t = &v->tuple3;
-      copy_ascii("(", sb);
+      StrBuilder_copyAscii(sb, "(");
       Debug_toStringHelp(depth - 1, t->a, sb);
-      copy_ascii(", ", sb);
+      StrBuilder_copyAscii(sb, ", ");
       Debug_toStringHelp(depth - 1, t->b, sb);
-      copy_ascii(", ", sb);
+      StrBuilder_copyAscii(sb, ", ");
       Debug_toStringHelp(depth - 1, t->c, sb);
-      copy_ascii(")", sb);
+      StrBuilder_copyAscii(sb, ")");
       return;
     }
     case Tag_Custom: {
       Custom* c = &v->custom;
       if (c == &True) {
-        copy_ascii("True", sb);
+        StrBuilder_copyAscii(sb, "True");
         return;
       } else if (c == &False) {
-        copy_ascii("False", sb);
+        StrBuilder_copyAscii(sb, "False");
         return;
       } else if (c == &Unit) {
-        copy_ascii("()", sb);
+        StrBuilder_copyAscii(sb, "()");
         return;
       }
       char* ctor;
@@ -148,12 +102,12 @@ void Debug_toStringHelp(int depth, void* p, StringBuilder* sb) {
       } else {
         ctor = "<unknown ctor>";
       }
-      copy_ascii(ctor, sb);
-      copy_ascii(" ", sb);
+      StrBuilder_copyAscii(sb, ctor);
+      StrBuilder_copyAscii(sb, " ");
       int len = custom_params(c);
       for (int i = 0; i < len; ++i) {
         Debug_toStringHelp(depth - 1, c->values[i], sb);
-        if (i != len - 1) copy_ascii(" ", sb);
+        if (i != len - 1) StrBuilder_copyAscii(sb, " ");
       }
       return;
     }
@@ -161,30 +115,30 @@ void Debug_toStringHelp(int depth, void* p, StringBuilder* sb) {
       Record* r = &v->record;
       FieldGroup* fg = r->fieldgroup;
       u32 size = fg->size;
-      copy_ascii("{", sb);
+      StrBuilder_copyAscii(sb, "{");
       for (int i = 0; i < size; ++i) {
         char* field = Debug_fields[fg->fields[i]];
-        copy_ascii(field, sb);
-        copy_ascii(": ", sb);
+        StrBuilder_copyAscii(sb, field);
+        StrBuilder_copyAscii(sb, ": ");
         Debug_toStringHelp(depth - 1, r->values[i], sb);
-        if (i != size - 1) copy_ascii(",", sb);
+        if (i != size - 1) StrBuilder_copyAscii(sb, ",");
       }
       return;
     }
     case Tag_FieldGroup: {
-      copy_ascii("<fieldgroup>", sb);
+      StrBuilder_copyAscii(sb, "<fieldgroup>");
       return;
     }
     case Tag_Closure: {
-      copy_ascii("<function>", sb);
+      StrBuilder_copyAscii(sb, "<function>");
       return;
     }
     case Tag_JsRef: {
-      copy_ascii("<JavaScript>", sb);
+      StrBuilder_copyAscii(sb, "<JavaScript>");
       return;
     }
     default: {
-      copy_ascii("<unknown>", sb);
+      StrBuilder_copyAscii(sb, "<unknown>");
       return;
     }
   }
@@ -192,27 +146,10 @@ void Debug_toStringHelp(int depth, void* p, StringBuilder* sb) {
 
 void* eval_Debug_toString(void* args[]) {
   void* value = args[0];
-  toString_alloc_chunk_words = 16;
-  size_t len = (toString_alloc_chunk_words - 1) * SIZE_UNIT / sizeof(u16);
-  ElmString16* str = newElmString16(len);
-  StringBuilder sb = {
-      .s = str,
-      .cursor = str->words16,
-      .end = str->words16 + len,
-  };
-
+  StrBuilder sb;
+  StrBuilder_init(&sb);
   Debug_toStringHelp(5, value, &sb);
-
-  // Shrink the string
-  ptrdiff_t cursor_addr = (ptrdiff_t)(sb.cursor);
-  ptrdiff_t aligned_cursor_addr = (cursor_addr + SIZE_UNIT - 1) & (-SIZE_UNIT);
-  ptrdiff_t size = (aligned_cursor_addr - (ptrdiff_t)str) / SIZE_UNIT;
-  str->header.size = (u32)size;
-
-  // Give back unused memory to the allocator
-  ptrdiff_t end_addr = (ptrdiff_t)(sb.end);
-  ptrdiff_t negative_alloc = aligned_cursor_addr - end_addr;
-  GC_allocate(false, negative_alloc / SIZE_UNIT);
+  ElmString16* str = StrBuilder_toString(&sb);
 
   return str;
 }
