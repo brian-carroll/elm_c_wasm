c6ca5bf37f18b792dd883c8ecf74d9109c42b76c
Brian Carroll <brian.carroll.ireland@gmail.com>
2021-06-25 06:36:33 +0100

    commit JS version of effects test, with debug modifications

--- demos/effects-test/src/debug.js
+++ demos/effects-test/src/debug.js
@@ -1,43 +1,96 @@
 function debug_step(proc) {
-  const TASK_SUCCEED = 0;
-  const TASK_FAIL = 1;
-  const TASK_BINDING = 2;
-  const TASK_AND_THEN = 3;
-  const TASK_ON_ERROR = 4;
-  const TASK_RECEIVE = 5;
-
-  const value = 'a';
-  const callback = 'b'
-  const kill = 'c'
-  const task = 'd'
   const id = 'e';
   const root_ = 'f';
   const stack = 'g';
   const mailbox = 'h';
 
-
   const root = proc[root_];
-  switch (root.$) {
-    case TASK_SUCCEED:
-      console.log(`Stepping Process ${proc[id]}: TASK_SUCCEED\t`, root[value]);
-      break;
-    case TASK_FAIL:
-      console.log(`Stepping Process ${proc[id]}: TASK_FAIL\t`, root[value]);
-      break;
-    case TASK_BINDING:
-      console.log(`Stepping Process ${proc[id]}: TASK_BINDING\t%s`, root[callback]);
-      break;
-    case TASK_AND_THEN:
-      console.log(`Stepping Process ${proc[id]}: TASK_AND_THEN\t%s`, root[callback]);
-      break;
-    case TASK_ON_ERROR:
-      console.log(`Stepping Process ${proc[id]}: TASK_ON_ERROR\t%s`, root[callback]);
-      break;
-    case TASK_RECEIVE:
-      console.log(`Stepping Process ${proc[id]}: TASK_RECEIVE\t%s`, root[callback]);
-      break;
-    default:
-      throw new Error(`Unknown Task ctor ` + proc[root]);
+  const procRoot = new Task(root);
+  console.log(`Stepping Process ${proc[id]}: root=\n`, procRoot, procRoot.toString());
+}
+
+class Task {
+  constructor(t) {
+    const value = 'a';
+    const callback = 'b';
+    const kill = 'c';
+    const task = 'd';
+
+    const ctors = [
+      'SUCCEED',
+      'FAIL',
+      'BINDING',
+      'AND_THEN',
+      'ON_ERROR',
+      'RECEIVE'
+    ];
+
+    this.$ = ctors[t.$];
+    this.value = t[value];
+    this.callback = t[callback];
+    this.kill = t[kill];
+    this.task = t[task] && new Task(t[task]);
   }
-  console.log(root);
+
+  toString(i = 0) {
+    const indent = ' '.repeat(i);
+    return `
+${indent}{
+${indent}  $: ${this.$}
+${indent}  value: ${this.value}
+${indent}  callback: ${this.callback}
+${indent}  kill: ${this.kill}
+${indent}  task: ${this.task && this.task.toString(i + 4)}
+${indent}}`;
+  }
+}
+
+function _Scheduler_step(proc)
+{
+	while (proc.f)
+	{
+    debug_step(proc);
+
+		var rootTag = proc.f.$;
+		if (rootTag === 0 || rootTag === 1)
+		{
+			while (proc.g && proc.g.$ !== rootTag)
+			{
+				proc.g = proc.g.i;
+			}
+			if (!proc.g)
+			{
+				return;
+			}
+			proc.f = proc.g.b(proc.f.a);
+			proc.g = proc.g.i;
+		}
+		else if (rootTag === 2)
+		{
+			proc.f.c = proc.f.b(function(newRoot) {
+				proc.f = newRoot;
+				_Scheduler_enqueue(proc);
+			});
+			return;
+		}
+		else if (rootTag === 5)
+		{
+			if (proc.h.length === 0)
+			{
+				return;
+			}
+      var received_msg = proc.h.shift();
+      console.log({received_msg})
+			proc.f = proc.f.b(received_msg);
+		}
+		else // if (rootTag === 3 || rootTag === 4)
+		{
+			proc.g = {
+				$: rootTag === 3 ? 0 : 1,
+				b: proc.f.b,
+				i: proc.g
+			};
+			proc.f = proc.f.d;
+		}
+	}
 }
